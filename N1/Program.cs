efae21ddcd58d4c96a7b88d164bd799a13814918
lolmem745 <93839006+lolmem745@users.noreply.github.com>
2022-11-08 15:31:46 +0300

    First problem solved

--- N1/Program.cs
+++ N1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int pow(int num1, int num2)
+{
+    int result = num1;
+    for (int i = 1; i < num2; i++)
+    {
+        result = result * num1;
+    }
+    return result;
+}
+Console.WriteLine("Введите число которое хотите возвести в степень, а затем степень в которую хотите возвести.");
+Console.WriteLine($"Результат: {pow(Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()))}");
