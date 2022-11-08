b3cb174b866da6be4064b5c97bc80aab801ca683
lolmem745 <93839006+lolmem745@users.noreply.github.com>
2022-11-08 16:53:37 +0300

    Third problem solved

--- N3/Program.cs
+++ N3/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] createArr(int arrLength)
+{
+    int[] arr = new int[arrLength];
+    return arr;
+}
+int[] arrFilling(int[] arr, int minValue, int maxValue)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(minValue, maxValue);
+    }
+    return arr;
+}
+void printArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}");
+        if (i != arr.Length - 1)
+            Console.Write(", ");
+    }
+    Console.Write("]");
+}
+
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите минимальное число в массиве: ");
+int minValue = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимальное число в массиве: ");
+int maxValue = Convert.ToInt32(Console.ReadLine()) + 1;
+printArray(arrFilling(createArr(size), minValue, maxValue));
